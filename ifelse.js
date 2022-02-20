11c47b8b9c50b126276b6a135f59c2fe9b0438ec
Aswinkl71 <99712645+aswinaswinkl@users.noreply.github.com>
2022-02-20 10:07:28 +0530

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,24 @@
+/*let a =0;
+if(a >0){
+    console.log("+ve");
+}else if(a < 0){
+    console.log("-ve");
+}else{
+    console.log("zero");
+}/*/
+let age = 20;
+if(age >=18)
+{
+    alert("eligible for driving license");
+}else{
+    alert("not eligible for driving license!!!");
+}
+
+
+
+
+
+
+
+
+
